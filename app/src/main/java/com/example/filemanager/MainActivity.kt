ec1cb655e46a864f74804874d038437b26a81451
KabeerSohail <sohail.mohammad@wenable.com>
2024-04-29 21:03:13 +0530

    not working

--- app/src/main/java/com/example/filemanager/MainActivity.kt
+++ app/src/main/java/com/example/filemanager/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.filemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.helper.widget.Carousel.Adapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var lastKnownPath: String = "/"
+    private var adapter: FileSystemAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Create and set up the RecyclerView adapter
         val adapter = FileSystemAdapter(filePaths)
+        this.adapter = adapter
         val recyclerView: RecyclerView =  findViewById(R.id.files_recycler_view)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -22,7 +28,8 @@ class MainActivity : AppCompatActivity() {
             // Update the adapter with the contents of the clicked folder
             adapter.updateItems(
                 subFoldersAndFiles.map {
-                    FileSystemAdapter.FolderX(name = it, path = "${itemName.path}/$it")
+                    lastKnownPath = itemName.path
+                    FileSystemAdapter.FolderX(name = it, path = "${itemName.path}/$it/")
                 }
             )
         }
@@ -49,5 +56,23 @@ class MainActivity : AppCompatActivity() {
         return immediateSubFoldersAndFiles
     }
 
+    override fun onBackPressed() {
+        // Check if current path is not root, then navigate back
+        if (lastKnownPath != "/") {
+            val parentFolderPath = lastKnownPath.substringBeforeLast("/")
+            lastKnownPath = parentFolderPath
+
+            // Fetch sub-folders and files for the parent folder
+            val parentSubFoldersAndFiles = fetchSubFoldersAndFiles(parentFolderPath)
+            // Update the adapter with the contents of the parent folder
+            adapter?.updateItems(
+                parentSubFoldersAndFiles.map {
+                    FileSystemAdapter.FolderX(name = it, path = "$parentFolderPath/$it")
+                }
+            )
+        } else {
+            super.onBackPressed() // Call default behavior if already at root
+        }
+    }
 
 }
